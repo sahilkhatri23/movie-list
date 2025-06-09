3ebc9abb29ea4cace7d4275e89612aa1b4d7eb03
Sahil Khatri <sahilkhatri152@gmail.com>
2025-06-09 14:38:12 +0530

    resolved build failure issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './css/App.css'
 import Favorites from './pages/Favorites';
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
-import NavBar from './components/Navbar';
+import NavBar from './components/NavBar';
 import { MovieProvider } from './contexts/MovieContext';
 
 function App() {
